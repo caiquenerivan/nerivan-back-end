04e18f5c028464c01c3512bee05fbfda6febf73e
Caique Nerivan <caiquenerivansantos@gmail.com>
2025-02-21 17:10:25 -0300

    Corrigindo conexão da api

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,6 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/authRoutes";
 import userRoutes from "./routes/userRoutes";
 import { randomBytes } from "crypto";
-import { log } from "console";
 
 dotenv.config();
 
@@ -17,19 +16,29 @@ const apiKeyGenerator = randomBytes(32).toString('hex');
 
 const server = fastify({ logger: true });
 const API_KEY = process.env.API_KEY;
+const allowedOrigins = [
+  'https://nerivan.com.br',
+  'https://www.nerivan.com.br',
+  'http://localhost:3000',
+  'http://localhost:5173',
+];
+server.register(cors, {
+  origin: allowedOrigins, // URL exata do seu frontend
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Inclua OPTIONS
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-api-key'],
+  exposedHeaders: ['Authorization'],
+  credentials: true, // Se estiver usando cookies/tokens,
+  maxAge: 86400
+})
 
 server.addHook('onRequest', async (request: FastifyRequest, reply: FastifyReply) => {
   const apiKey = request.headers['x-api-key'];
   if (!apiKey || apiKey !== API_KEY) {
-    return reply.code(401).send({error: "Unauthorized"})
-  }
+    return reply.code(401).send({ error: "Unauthorized" })
+  } 
 });
 
-const allowedOrigins = [
-  'https://nerivan.com.br',
-  'http://localhost:3000',
-  'http://localhost:5173',
-];
+
 
 // Registrar rotas
 server.register(skillRoutes);
@@ -38,23 +47,19 @@ server.register(workTypeRoutes);
 server.register(authRoutes);
 server.register(userRoutes);
 
-server.register(cors, {
-  origin: allowedOrigins, // Permite todas as origens (não recomendado para produção)
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
-});
-server.get('/', async (request, reply) => {
-  return { message: 'Hello, world!' };
-});
 
-const port = process.env.PORT || 3000;
+
+const port = 3000;
 
 
 const start = async () => {
   try {
-    await server.listen({ host: '0.0.0.0', port: Number(port) });
-    console.log(`Server listening on port ${port}`);
+    await server.listen({ host: '0.0.0.0', port: Number(port) }, (err) => {
+      if (err) throw err
+      console.log(`Server listening on port ${port}`);
+    });
     //console.log(apiKeyGenerator);
-    
+
   } catch (err) {
     server.log.error(err);
     process.exit(1);
